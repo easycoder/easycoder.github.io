43e88496fde9f558fb616f53053c7308e2f25e26
Graham Trott <gt@pobox.com>
2021-10-30 12:13:51 +0100

    Change CDN

--- dist/easycoder.php
+++ dist/easycoder.php
@@ -16,7 +16,7 @@
   add_action('wp_enqueue_scripts', 'easycoder_enqueue_script', 2);
   function easycoder_enqueue_script() {  
     wp_enqueue_script('easycoder_script',
-      'https://cdn.jsdelivr.net/gh/easycoder/easycoder.github.io/dist/easycoder.js', array(), '2.7.8');
+      'https://easycoder.netlify.app/dist/easycoder.js', array(), '2.7.8');
   }
   
   // Set up default plugin and REST scripts
