29753e5407b5adfbb8a201d96da2b2a6950f06be
Graham <gt@pobox.com>
2023-03-26 22:09:28 +0100

    DST adjust

--- dist/easycoder.js
+++ dist/easycoder.js
@@ -2905,7 +2905,7 @@ const EasyCoder_Core = {
 					content: ``
 				};
 			case `now`:
-                const d = Date();
+                const d = new Date();
                 const jan = new Date(d.getFullYear(), 0, 1).getTimezoneOffset();
                 const jul = new Date(d.getFullYear(), 6, 1).getTimezoneOffset();
                 const isDST = Math.max(jan, jul) !== d.getTimezoneOffset();  
