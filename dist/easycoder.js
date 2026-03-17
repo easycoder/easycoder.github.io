0f5380142006b8924267ceae1831f2721f78511d
Graham <gt@pobox.com>
2026-03-17 08:16:15 +0000

    260317.1

--- dist/easycoder.js
+++ dist/easycoder.js
@@ -9633,14 +9633,17 @@ const EasyCoder_REST = {
 					}
 					path = `${window.location.origin}${url}`;
 				} else {
-					const error = `Relative REST endpoints are disabled in static hosting mode`;
-					if (command.onError) {
-						program.errorMessage = error;
-						program.run(command.onError);
+					if (command.request === `post`) {
+						const error = `Relative REST POST endpoints are disabled in static hosting mode`;
+						if (command.onError) {
+							program.errorMessage = error;
+							program.run(command.onError);
+							return 0;
+						}
+						program.runtimeError(command.lino, error);
 						return 0;
 					}
-					program.runtimeError(command.lino, error);
-					return 0;
+					path = url;
 				}
 			}
 
